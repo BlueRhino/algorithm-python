1fa4ce890cc0542ddeff69adfcd8fe2a703deae3
bluerhino <nie_kl@126.com>
2017-07-03 23:27:11 +0800

    Finish the max heap sort.

--- sort/heapsort/heap_sort.py
+++ sort/heapsort/heap_sort.py
@@ -25,49 +25,75 @@ def check_is_max_heap(arr):
 
 def check_subtree_is_max_heap(arr, index):
     """
-    判断某子树是不是最大堆
+    判断数组arr的index节点为根的子树是否为大根堆
     :param arr: 数组存储的二叉树
     :param index:子树的跟
     :return: 
     """
     arr_len = len(arr)
+    return check_subtree_is_max_heap_with_len(arr, index, arr_len)
+
+
+def check_subtree_is_max_heap_with_len(arr, index, arr_len):
+    """
+    判断数组arr的index节点为根的子树是否为大根堆，只处理前arr_len个节点
+    :param arr_len:判断长度
+    :param arr: 数组存储的二叉树
+    :param index:子树的跟
+    :return:
+    """
     if index >= arr_len:
-        return False
+        return True
     left_root = index * 2 + 1
     right_root = index * 2 + 2
     if right_root >= arr_len:
         return True
-    if left_root < arr_len & arr[left_root] > arr[index]:
+    if left_root < arr_len and arr[left_root] > arr[index]:
         return False
-    if right_root < arr_len & arr[right_root] > arr[index]:
+    if right_root < arr_len and arr[right_root] > arr[index]:
         return False
-    return check_subtree_is_max_heap(arr, left_root) & check_subtree_is_max_heap(arr, right_root)
+    left_check = check_subtree_is_max_heap_with_len(arr, left_root, arr_len)
+    right_check = check_subtree_is_max_heap_with_len(arr, right_root, arr_len)
+    return left_check and right_check
 
 
-def max_heapify(arr_a, index):
+def max_heapify(arr, index):
     """
-    :param arr_a: 需要调整的数组，数组满足假设，以index为根的节点的左右子树满足大根堆定义
+    将arr数组index节点置为大根堆
+    :param arr: 需要调整的数组，数组满足假设，以index为根的节点的左右子树满足大根堆定义
     :param index: 需要调整的节点
     :return: 
     """
+    arr_len = len(arr)
+    return max_heapify_with_len(arr, index, arr_len)
+
+
+def max_heapify_with_len(arr, index, arr_len):
+    """
+    将arr数组index节点置为大根堆，只处理前arr_len个节点
+    :param arr_len:
+    :param arr: 需要调整的数组，数组满足假设，以index为根的节点的左右子树满足大根堆定义
+    :param index: 需要调整的节点
+    :return:
+    """
     left_root = index * 2 + 1
     right_root = index * 2 + 2
-    if check_subtree_is_max_heap(left_root) & check_subtree_is_max_heap(right_root):
+    if not ((check_subtree_is_max_heap_with_len(arr, left_root, arr_len))
+            and (check_subtree_is_max_heap_with_len(arr, right_root, arr_len))):
         raise Exception('The subtree is not a max heap')
-    arr_len = len(arr_a)
     largest = index
     if left_root < arr_len:
-        if arr_a[left_root] > arr_a[index]:
+        if arr[left_root] > arr[index]:
             largest = left_root
     if right_root < arr_len:
-        if arr_a[right_root] > arr_a[largest]:
+        if arr[right_root] > arr[largest]:
             largest = right_root
     if largest != index:
-        temp = arr_a[index]
-        arr_a[index] = arr_a[largest]
-        arr_a[largest] = temp
-        arr_a = max_heapify(arr_a, largest)
-    return arr_a
+        temp = arr[index]
+        arr[index] = arr[largest]
+        arr[largest] = temp
+        arr = max_heapify_with_len(arr, largest, arr_len)
+    return arr
 
 
 def build_max_heap(arr):
@@ -82,4 +108,21 @@ def build_max_heap(arr):
     last_parent_index = int(math.floor(arr_len / 2)) - 1
     for i in range(last_parent_index, -1, -1):
         arr = max_heapify(arr, i)
+    return arr
+
+
+def max_heap_sort(arr):
+    """
+    根据输入的数组进行堆排序
+    :param arr: 输入的无序数组
+    :return: 输出按照元素大小由小到大的排序后数组
+    """
+    arr = build_max_heap(arr)
+    arr_len = len(arr)
+    for i in range(arr_len - 1, -1, -1):
+        # 根为第一个元素，必为最大
+        tmp = arr[0]
+        arr[0] = arr[i]
+        arr[i] = tmp
+        max_heapify_with_len(arr, 0, i)
     return arr
--- test/sort/heapsort/test_heap_sort.py
+++ test/sort/heapsort/test_heap_sort.py
@@ -3,35 +3,40 @@ from unittest import TestCase
 from sort.heapsort import heap_sort
 
 
-class TestBuildMaxHeap(TestCase):
+class TestHeapSort(TestCase):
     def test_max_heapify(self):
         res = heap_sort.max_heapify([16, 4, 10, 14, 7, 9, 3, 2, 8, 1], 1)
-        self.assertEquals(res, [16, 14, 10, 8, 7, 9, 3, 2, 4, 1])
+        self.assertEqual(res, [16, 14, 10, 8, 7, 9, 3, 2, 4, 1])
 
     def test_build_max_heap(self):
         arr = heap_sort.build_max_heap([4, 1, 3, 2, 16, 9, 10, 14, 8, 7])
-        self.assertEquals(arr, [16, 14, 10, 8, 7, 9, 3, 2, 4, 1])
+        self.assertEqual(arr, [16, 14, 10, 8, 7, 9, 3, 2, 4, 1])
 
     def test_build_max_heap_one(self):
         arr = heap_sort.build_max_heap([4])
-        self.assertEquals(arr, [4])
+        self.assertEqual(arr, [4])
 
     def test_build_max_heap_zero(self):
         arr = heap_sort.build_max_heap([])
-        self.assertEquals(arr, [])
+        self.assertEqual(arr, [])
 
     def test_check_is_max_heap(self):
         res = heap_sort.check_is_max_heap([4])
-        self.assertEquals(res, True)
+        self.assertEqual(res, True)
         res = heap_sort.check_is_max_heap([4, 1, 2])
-        self.assertEquals(res, True)
+        self.assertEqual(res, True)
         res = heap_sort.check_is_max_heap([4, 1, 2, 6])
-        self.assertEquals(res, False)
+        self.assertEqual(res, False)
 
     def test_check_subtree_is_max_heap(self):
         res = heap_sort.check_subtree_is_max_heap([16, 4, 10, 14, 7, 9, 3, 2, 8, 1], 1)
-        self.assertEquals(res, False)
+        self.assertEqual(res, False)
         res = heap_sort.check_subtree_is_max_heap([16, 4, 10, 14, 7, 9, 3, 2, 8, 1], 3)
-        self.assertEquals(res, True)
+        self.assertEqual(res, True)
         res = heap_sort.check_subtree_is_max_heap([16, 4, 10, 14, 7, 9, 3, 2, 8, 1], 4)
-        self.assertEquals(res, True)
+        self.assertEqual(res, True)
+
+    def test_max_heap_sort(self):
+        arr = [16, 14, 10, 8, 7, 9, 3, 2, 4, 1]
+        res = heap_sort.max_heap_sort(arr)
+        self.assertEqual(res, [1, 2, 3, 4, 7, 8, 9, 10, 14, 16])
