866e7fccf19a50f1274ce84727cc5cd975881ad3
bluerhino <nie_kl@126.com>
2017-07-05 21:43:57 +0800

    add tow function to priority queue.

--- sort/heapsort/priority_queue.py
+++ sort/heapsort/priority_queue.py
@@ -1,3 +1,5 @@
+import math
+
 from sort.heapsort.heap_sort import build_max_heap, max_heapify_with_len
 
 
@@ -29,6 +31,42 @@ class PriorityQueue:
         self.max_heap[0] = self.max_heap[heap_size - 1]
         self.max_heap = max_heapify_with_len(self.max_heap, 0, heap_size - 1)[0:heap_size - 1]
         return max_element
+
+    def increase_value(self, index, new_value):
+        """
+        将index的位置的元素置为新的值并保持最大堆定义，新值必须大于原值
+        :param index:
+        :param new_value:
+        :return:
+        """
+        self.__check_heap_not_empty()
+        if index < 0:
+            raise Exception("The index must be an integer greater than zero.")
+        if index > (len(self.max_heap) - 1):
+            raise Exception("Index out of range.Index is:" + str(index)
+                            + ".And size of arr is:" + str(len(self.max_heap)))
+        old_value = self.max_heap[index]
+        if old_value > new_value:
+            raise Exception("The new value must greater than the old one.")
+        self.max_heap[index] = new_value
+        while index > 0:
+            parent_index = math.floor((index - 1) / 2)
+            if self.max_heap[index] > self.max_heap[parent_index]:
+                tmp = self.max_heap[parent_index]
+                self.max_heap[parent_index] = self.max_heap[index]
+                self.max_heap[index] = tmp
+                index = parent_index
+            else:
+                return
+
+    def insert_value(self, value):
+        """
+        插入新节点到最大堆中，并且保持最大堆定义
+        :param value:
+        :return:
+        """
+        self.max_heap.append(value)
+        self.increase_value(len(self.max_heap) - 1, value)
 
     def __check_heap_not_empty(self):
         if len(self.max_heap) <= 0:
--- test/sort/heapsort/test_priorityQueue.py
+++ test/sort/heapsort/test_priorityQueue.py
@@ -15,3 +15,20 @@ class TestPriorityQueue(TestCase):
         priority_queue = PriorityQueue(arr)
         self.assertEqual(priority_queue.extract_max(), 16)
         self.assertEqual(check_is_max_heap(priority_queue.get_max_heap()), True)
+
+    def test_increase_value(self):
+        arr = [16, 14, 10, 8, 7, 9, 3, 2, 4, 1]
+        priority_queue = PriorityQueue(arr)
+        priority_queue.increase_value(8, 15)
+        self.assertEqual(priority_queue.get_max_heap(),
+                         [16, 15, 10, 14, 7, 9, 3, 2, 8, 1])
+
+    def test_insert_value(self):
+        arr = [16, 14, 10, 8, 7, 9, 3, 2, 4, 1]
+        priority_queue = PriorityQueue(arr)
+        priority_queue.insert_value(2)
+        self.assertEqual(priority_queue.get_max_heap(),
+                         [16, 14, 10, 8, 7, 9, 3, 2, 4, 1, 2])
+        priority_queue.insert_value(20)
+        self.assertEqual(priority_queue.get_max_heap(),
+                         [20, 14, 16, 8, 7, 10, 3, 2, 4, 1, 2, 9])
